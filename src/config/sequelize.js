10ea0a01ccfea0674984e8131e631bb06f1f217f
PrathapAI <168130563+PrathapAI@users.noreply.github.com>
2025-08-05 09:06:57 +0530

    Update sequelize.js

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -3,11 +3,11 @@ import { Sequelize } from 'sequelize';
 
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DB || 'postgres',
-  process.env.POSTGRES_USER || 'postgres',
-  process.env.POSTGRES_PASSWORD || 'password',
+  process.env.POSTGRES_DB || 'postgress_r037',
+  process.env.POSTGRES_USER || 'postgress_r037_user',
+  process.env.POSTGRES_PASSWORD || 'ChjBYwsxjbQDwQiRLpeQtHhGHR5vpJbq',
   {
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST || 'dpg-d22v5i6mcj7s73d1090g-a',
     dialect: 'postgres',
     port: process.env.POSTGRES_PORT || 5432,
     logging: console.log, // Enable logging to see SQL queries and errors
